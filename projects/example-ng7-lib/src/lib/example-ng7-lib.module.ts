2cdd1df285c38e0356a32367559e02f01236bb0b
dharmen.s <dharmen.s@milestoneinternet.info>
2018-12-07 18:11:13 +0530

    [CORE]-scroll, handle position

--- projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
+++ projects/example-ng7-lib/src/lib/example-ng7-lib.module.ts
@@ -4,12 +4,14 @@ import { FooComponent } from './foo/foo.component';
 import { BarDirective } from './foo/bar.directive';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @NgModule({
   declarations: [ExampleNg7LibComponent, FooComponent, BarDirective],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    NgScrollbarModule
   ],
   exports: [ExampleNg7LibComponent, FooComponent, BarDirective]
 })
--- projects/example-ng7-lib/src/lib/foo/foo.component.ts
+++ projects/example-ng7-lib/src/lib/foo/foo.component.ts
@@ -1,23 +1,91 @@
-import { Component, OnInit, Input, QueryList, ContentChildren, AfterContentInit, ContentChild, ElementRef } from '@angular/core';
+import {
+  Component, OnInit, Input, QueryList, ContentChildren, AfterContentInit, ElementRef, ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { FooOptions } from './foo-options';
 import { BarDirective } from './bar.directive';
+import { NgScrollbar } from 'ngx-scrollbar';
 
 @Component({
   selector: 'enl-foo',
   templateUrl: './foo.component.html',
   styleUrls: ['./foo.component.scss']
 })
-export class FooComponent implements OnInit, AfterContentInit {
+export class FooComponent implements OnInit, AfterContentInit, AfterViewInit {
   objectKeys = Object.keys;
   @Input() options?: FooOptions;
   @ContentChildren(BarDirective) barDirectives !: QueryList<BarDirective>;
-  @ContentChild('handle') handle: ElementRef;
+  @ViewChild('handle') handle: ElementRef;
+  @ViewChild('handleParent') handleParent: ElementRef;
+  @ViewChild(NgScrollbar) scrollBar: NgScrollbar;
   numberOfBars: number;
   lastChild: BarDirective;
   firsts: { [x: string]: string } = {};
+  executed = false;
+  ticking = false;
+  currentInEvent: { in: string; out: string; };
+  currentOutEvent: { in: string; out: string; };
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    [{ in: 'mousedown', out: 'mouseup' },
+    { in: 'mouseover', out: 'mouseleave' }].forEach(ev => {
+      this.scrollBar.verticalScrollbar.nativeElement.addEventListener(ev.in, _ => {
+        if (!this.currentOutEvent || (this.currentOutEvent.out === 'mousedown' && this.currentOutEvent.out === ev.out)) {
+          this.currentInEvent = ev;
+          this.handle.nativeElement.classList.add('show');
+        }
+      }, { passive: true });
+      this.scrollBar.verticalScrollbar.nativeElement.addEventListener(ev.out, () => {
+        if (ev.out === this.currentInEvent.out) {
+          this.currentOutEvent = ev;
+          this.handle.nativeElement.classList.remove('show');
+        }
+      }, { passive: true });
+    });
+    this.handle.nativeElement.addEventListener('wheel', (_e: WheelEvent) => {
+      if (_e.cancelable) {
+        const top = this.scrollBar.view.scrollTop;
+        this.handleEvent(this.scrollBar, this.scrollBar.scrollYTo, [top + (_e.deltaY < 0 ? -200 : 200), 150]);
+      }
+    }, { passive: true });
+    this.handleParent.nativeElement.addEventListener('wheel', (_e: WheelEvent) => {
+      if (_e.cancelable) {
+        const top = this.scrollBar.view.scrollTop;
+        this.handleEvent(this.scrollBar, this.scrollBar.scrollYTo, [top + (_e.deltaY < 0 ? -200 : 200), 150]);
+      }
+    }, { passive: true });
+    this.scrollBar.view.addEventListener('scroll', _e => {
+      _e.preventDefault();
+      const last_known_scroll_position = this.scrollBar.view.scrollTop;
+      this.handleEvent(this, this.handlePosition, [last_known_scroll_position]);
+    }, { passive: true });
+  }
+
+  private handleEvent(thisScope: any, functionToBeExecuted: Function, args: any[]) {
+    if (!this.executed) {
+      this.executed = true;
+      if (!this.ticking) {
+        requestAnimationFrame(_ => {
+          functionToBeExecuted.apply(thisScope, args);
+          this.ticking = false;
+        });
+        this.ticking = true;
+      }
+      setTimeout(() => {
+        this.executed = false;
+      });
+    }
+  }
+
+  private handlePosition(last_known_scroll_position: any) {
+    const offsetHeight = this.handleParent.nativeElement.offsetHeight;
+    const scrollHeight = this.scrollBar.view.scrollHeight;
+    const scrolledPercent = last_known_scroll_position / scrollHeight * 100;
+    this.handle.nativeElement.style.top = (scrolledPercent / 100 * offsetHeight < 6 ? 6 : scrolledPercent / 100 * offsetHeight) + 'px';
   }
 
   ngAfterContentInit() {
