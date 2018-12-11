9148b2c7b11a5570b0683bb9e7626168ce2efbc8
dharmen.s <dharmen.s@milestoneinternet.info>
2018-12-11 19:08:17 +0530

    [CORE]-basics achieved

--- projects/example-ng7-lib/src/lib/foo/bar.directive.ts
+++ projects/example-ng7-lib/src/lib/foo/bar.directive.ts
@@ -1,10 +1,22 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, Input, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[enlBar]'
 })
 export class BarDirective {
   @Input('enlBar') barName: string;
-  constructor() { }
+  constructor(private ele: ElementRef) { }
+
+  get character() {
+    return this.barName.charAt(0).toUpperCase();
+  }
+
+  get offsetTop() {
+    return this.ele.nativeElement.offsetTop;
+  }
+
+  get offsetHeight() {
+    return this.ele.nativeElement.offsetHeight;
+  }
 
 }
--- projects/example-ng7-lib/src/lib/foo/foo.component.ts
+++ projects/example-ng7-lib/src/lib/foo/foo.component.ts
@@ -15,81 +15,58 @@ export class FooComponent implements OnInit, AfterContentInit, AfterViewInit {
   objectKeys = Object.keys;
   @Input() options?: FooOptions;
   @ContentChildren(BarDirective) barDirectives !: QueryList<BarDirective>;
-  @ViewChild('handle') handle: ElementRef;
-  @ViewChild('handleParent') handleParent: ElementRef;
   @ViewChild(NgScrollbar) scrollBar: NgScrollbar;
   numberOfBars: number;
-  lastChild: BarDirective;
-  firsts: { [x: string]: string } = {};
-  executed = false;
+  firsts: BarDirective[] = [];
   ticking = false;
-  currentInEvent: { in: string; out: string; };
-  currentOutEvent: { in: string; out: string; };
   constructor() { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    [{ in: 'mousedown', out: 'mouseup' },
-    { in: 'mouseover', out: 'mouseleave' }].forEach(ev => {
-      this.scrollBar.verticalScrollbar.nativeElement.addEventListener(ev.in, _ => {
-        if (!this.currentOutEvent || (this.currentOutEvent.out === 'mousedown' && this.currentOutEvent.out === ev.out)) {
-          this.currentInEvent = ev;
-          this.handle.nativeElement.classList.add('show');
-        }
-      }, { passive: true });
-      this.scrollBar.verticalScrollbar.nativeElement.addEventListener(ev.out, () => {
-        if (ev.out === this.currentInEvent.out) {
-          this.currentOutEvent = ev;
-          this.handle.nativeElement.classList.remove('show');
-        }
-      }, { passive: true });
-    });
-    this.handle.nativeElement.addEventListener('wheel', (_e: WheelEvent) => {
-      if (_e.cancelable) {
-        const top = this.scrollBar.view.scrollTop;
-        this.handleEvent(this.scrollBar, this.scrollBar.scrollYTo, [top + (_e.deltaY < 0 ? -200 : 200), 150]);
-      }
-    }, { passive: true });
-    this.handleParent.nativeElement.addEventListener('wheel', (_e: WheelEvent) => {
-      if (_e.cancelable) {
-        const top = this.scrollBar.view.scrollTop;
-        this.handleEvent(this.scrollBar, this.scrollBar.scrollYTo, [top + (_e.deltaY < 0 ? -200 : 200), 150]);
-      }
-    }, { passive: true });
+    let timer = null;
     this.scrollBar.view.addEventListener('scroll', _e => {
       _e.preventDefault();
-      const last_known_scroll_position = this.scrollBar.view.scrollTop;
-      this.handleEvent(this, this.handlePosition, [last_known_scroll_position]);
+      if (timer !== null) {
+        this.scrollBar.verticalScrollbar.nativeElement.getElementsByClassName('scroll-thumbs')[0].classList.add('show');
+        this.handleEvent(this, this.handleText);
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        this.scrollBar.verticalScrollbar.nativeElement.getElementsByClassName('scroll-thumbs')[0].classList.remove('show');
+      }, 500);
     }, { passive: true });
   }
 
-  private handleEvent(thisScope: any, functionToBeExecuted: Function, args: any[]) {
-    if (!this.executed) {
-      this.executed = true;
-      if (!this.ticking) {
-        requestAnimationFrame(_ => {
-          functionToBeExecuted.apply(thisScope, args);
-          this.ticking = false;
-        });
-        this.ticking = true;
-      }
-      setTimeout(() => {
-        this.executed = false;
+
+  // a generic event handler, to get rid of verbose/browser errors
+  // https://developer.mozilla.org/en-US/docs/Web/Events/scroll#Example
+  private handleEvent(thisScope: any, functionToBeExecuted: Function) {
+    if (!this.ticking) {
+      requestAnimationFrame(_ => {
+        functionToBeExecuted.apply(thisScope);
+        this.ticking = false;
       });
+      this.ticking = true;
     }
   }
 
-  private handlePosition(last_known_scroll_position: any) {
-    const offsetHeight = this.handleParent.nativeElement.offsetHeight;
-    const scrollHeight = this.scrollBar.view.scrollHeight;
-    const scrolledPercent = last_known_scroll_position / scrollHeight * 100;
-    this.handle.nativeElement.style.top = (scrolledPercent / 100 * offsetHeight < 6 ? 6 : scrolledPercent / 100 * offsetHeight) + 'px';
+  private handleText() {
+    const scrollTop = this.scrollBar.view.scrollTop;
+    const windowHeight = this.scrollBar.view.offsetHeight;
+    this.firsts.find(item => {
+      const offsetTop = item.offsetTop;
+      const offsetHeight = item.offsetHeight;
+      const condition = scrollTop <= offsetTop && (offsetHeight + offsetTop) < (scrollTop + windowHeight);
+      if (condition) {
+        this.scrollBar.verticalScrollbar.nativeElement.getElementsByClassName('scroll-thumbs')[0].setAttribute('letter', item.character);
+        return condition;
+      }
+    });
   }
 
   ngAfterContentInit() {
-    this.lastChild = this.barDirectives.last;
     this.calculateLength();
     this.barDirectives.changes.subscribe(_ => {
       this.calculateLength();
@@ -100,10 +77,9 @@ export class FooComponent implements OnInit, AfterContentInit, AfterViewInit {
     setTimeout(_ => {
       this.numberOfBars = this.barDirectives.length;
       this.barDirectives.forEach(item => {
-        if (!this.firsts[item.barName.charAt(0).toUpperCase()]) {
-          this.firsts[item.barName.charAt(0).toUpperCase()] = item.barName;
-        }
+        this.firsts.push(item);
       });
+      this.handleText();
     });
   }
 }
